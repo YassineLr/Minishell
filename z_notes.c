2baf2e4ee4863a3e60b0c08eb9fde79bc1d28b91
Yassine Larhrissi <72478988+YassineLr@users.noreply.github.com>
2023-07-11 18:46:45 +0100

    Update z_notes.c

--- z_notes.c
+++ z_notes.c
@@ -1,3 +1,5 @@
 // env leaks
 // parser
 // remove spaces after operations
+// < ok cat
+// loop in lex_list and save redirections in the reds list, then save the last fd (in and out), and close other fds
