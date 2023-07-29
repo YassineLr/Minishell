8b796fe754a08f31aeb50ac6655928a0cbc7d217
Oussama Belhaj <oubelhaj@e1r1p1.1337.ma>
2023-07-29 09:24:46 +0100

    fixing ambiguous redirect

--- src/execution/execute_cmd.c
+++ src/execution/execute_cmd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute_cmd.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylarhris <ylarhris@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oubelhaj <oubelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 11:47:17 by ylarhris          #+#    #+#             */
-/*   Updated: 2023/07/28 03:10:52 by ylarhris         ###   ########.fr       */
+/*   Updated: 2023/07/29 09:20:19 by oubelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,37 @@
 
 char    *ft_path(t_parser *parse, t_env *env)
 {
-    char    *path;
-    char    *paths;
-    char    **splited;
-    int        i;
+	char    *path;
+	char    *paths;
+	char    **splited;
+	int        i;
 
-    i = 0;
-    paths = search_in_env(env, "PATH")->value;
-    if (!paths)
-    {
-        write (2, "No such file or directory\n", 27);
-        exit (127);
-    }
-    splited = ft_split(paths, ':');
-    // printf("it is splited\n");
-    while (splited[i])
-    {
-        path = ft_strjoin(ft_strjoin(splited[i], "/"), parse->command->cmds[0]);
-        if (access(path, X_OK | F_OK) == 0)
-            return (path);
-        i++;
-    }
-    write(2, "command not found\n", 19);
-    exit(127);
-    return (NULL);
+	i = 0;
+	paths = search_in_env(env, "PATH")->value;
+	if (!paths)
+	{
+		write (2, "No such file or directory\n", 27);
+		exit (127);
+	}
+	splited = ft_split(paths, ':');
+	// printf("it is splited\n");
+	while (splited[i])
+	{
+		path = ft_strjoin(ft_strjoin(splited[i], "/"), parse->command->cmds[0]);
+		if (access(path, X_OK | F_OK) == 0)
+			return (path);
+		i++;
+	}
+	write(2, "command not found\n", 19);
+	exit(127);
+	return (NULL);
 }
 
 // void open_pipes(t_parser *parse)
 // {
 //     t_parser *courant;
 //     int        T[2];
-    
+	
 //     courant = parse;
 //     while (courant->next)
 //     {
@@ -59,85 +59,85 @@ char    *ft_path(t_parser *parse, t_env *env)
 
 void init_fds(t_parser *parse)
 {
-    t_parser    *cur;
+	t_parser    *cur;
 
-    cur = parse;
-    while (cur)
-    {
-        // cur->command->red_in = 0;
-        // cur->command->red_out = 1;
-        cur->command->pipe_fd.read = 0;
-        cur->command->pipe_fd.write = 1;
-        cur->command->pipe_fd.to_close = 0;
-        cur = cur->next;
-    }
+	cur = parse;
+	while (cur)
+	{
+		// cur->command->red_in = 0;
+		// cur->command->red_out = 1;
+		cur->command->pipe_fd.read = 0;
+		cur->command->pipe_fd.write = 1;
+		cur->command->pipe_fd.to_close = 0;
+		cur = cur->next;
+	}
 }
 
 void    ft_dup(t_parser *parse)
 {
 
-    if (parse->command->pipe_fd.to_close && parse->command->pipe_fd.to_close != 1)
-        close(parse->command->pipe_fd.to_close);
-    if(parse->command->red_out != 1)
-        dup2(parse->command->red_out, STDOUT_FILENO);
-    if(parse->command->red_in != 0)
-        dup2(parse->command->red_in, STDIN_FILENO);
-    if(!parse->next)
-        if (parse->command->pipe_fd.write != 1)
-            close(parse->command->pipe_fd.write);
-    if (parse->command->pipe_fd.read)
-        dup2(parse->command->pipe_fd.read, STDIN_FILENO);
-    if (parse->command->pipe_fd.write != 1)
-        dup2(parse->command->pipe_fd.write, STDOUT_FILENO);
-     
+	if (parse->command->pipe_fd.to_close && parse->command->pipe_fd.to_close != 1)
+		close(parse->command->pipe_fd.to_close);
+	if(parse->command->red_out != 1)
+		dup2(parse->command->red_out, STDOUT_FILENO);
+	if(parse->command->red_in != 0)
+		dup2(parse->command->red_in, STDIN_FILENO);
+	if(!parse->next)
+		if (parse->command->pipe_fd.write != 1)
+			close(parse->command->pipe_fd.write);
+	if (parse->command->pipe_fd.read)
+		dup2(parse->command->pipe_fd.read, STDIN_FILENO);
+	if (parse->command->pipe_fd.write != 1)
+		dup2(parse->command->pipe_fd.write, STDOUT_FILENO);
+	 
 }
 
 char **env_in_tab(t_env *env)
 {
-    char    **envp;
-    int     count;
-    t_env   *cur;
-    int     i;
-    
-    i = 0;
-    count = 0;
-    cur = env;
-    while (cur)
-    {
-        count++;
-        cur = cur->next;
-    }
-    // exit(100);
-    envp = (char **) malloc((count+1)*sizeof(char *));
-    while (env)
-    {
-        envp[i] = ft_strjoin(env->key,"=");
-        envp[i] = ft_strjoin(envp[i],env->value);
-        env=env->next;
-        i++;
-    }
-    envp[count] = NULL;
-    return(envp);
+	char    **envp;
+	int     count;
+	t_env   *cur;
+	int     i;
+	
+	i = 0;
+	count = 0;
+	cur = env;
+	while (cur)
+	{
+		count++;
+		cur = cur->next;
+	}
+	// exit(100);
+	envp = (char **) malloc((count+1)*sizeof(char *));
+	while (env)
+	{
+		envp[i] = ft_strjoin(env->key,"=");
+		envp[i] = ft_strjoin(envp[i],env->value);
+		env=env->next;
+		i++;
+	}
+	envp[count] = NULL;
+	return(envp);
 }
 
 void set_pipes(t_parser *parse)
 {
-    t_parser     *courant;
-    int            T[2];
+	t_parser     *courant;
+	int            T[2];
 
-    
-    courant = parse;
-    while (courant->next)
-    {
-        if (courant->command->pipe == 1)
-        {
-            pipe(T);
-            courant->command->pipe_fd.write = T[1];
-            courant->next->command->pipe_fd.to_close = T[1];
-            courant->next->command->pipe_fd.read = T[0];
-        }
-        courant = courant->next;
-    }
+	
+	courant = parse;
+	while (courant->next)
+	{
+		if (courant->command->pipe == 1)
+		{
+			pipe(T);
+			courant->command->pipe_fd.write = T[1];
+			courant->next->command->pipe_fd.to_close = T[1];
+			courant->next->command->pipe_fd.read = T[0];
+		}
+		courant = courant->next;
+	}
 }
 
 void	exit_status(int status)
@@ -151,21 +151,21 @@ void	exit_status(int status)
 
 void   execute_cmd(t_parser *parse, t_env *env, char **envp, int id)
 {
-    char *path;
-    
-    ft_dup(parse);
-    if(!id)
-    {
-        execve(parse->command->cmds[0], parse->command->cmds, envp);
-        path = ft_path(parse, env);
-        if (!path)
-            exit(127);
-        if (execve(path, parse->command->cmds, envp) == -1)
-        {
-            write(2, "could not execve", 17);
-            exit(127);
-        }
-        exit(1);
-    }
+	char *path;
+	
+	ft_dup(parse);
+	if(!id)
+	{
+		execve(parse->command->cmds[0], parse->command->cmds, envp);
+		path = ft_path(parse, env);
+		if (!path)
+			exit(127);
+		if (execve(path, parse->command->cmds, envp) == -1)
+		{
+			write(2, "could not execve\n", 18);
+			exit(127);
+		}
+		exit(1);
+	}
 }
 
--- src/parsing/file_opening.c
+++ src/parsing/file_opening.c
@@ -3,19 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   file_opening.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ylarhris <ylarhris@student.42.fr>          +#+  +:+       +#+        */
+/*   By: oubelhaj <oubelhaj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 10:59:22 by oubelhaj          #+#    #+#             */
-/*   Updated: 2023/07/07 04:03:22 by ylarhris         ###   ########.fr       */
+/*   Updated: 2023/07/29 09:24:05 by oubelhaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+int	check_filename(char *name)
+{
+	// still testing
+	int	i;
+	
+	i = 0;
+	while (ft_is_whitespace(name[i]))
+		i++;
+	if (name[i] != '\0')
+		return (1);
+	return (0);
+}
+
 int	open_redin(char *filename)
 {
 	int	fd;
 
+	if (!check_filename(filename))
+	{
+	 	write(2, "minishell: ambiguous redirect\n", 31);
+		return (-1);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
@@ -29,6 +47,11 @@ int	open_redout(char *filename)
 {
 	int	fd;
 
+	if (!check_filename(filename))
+	{
+	 	write(2, "minishell: ambiguous redirect\n", 31);
+		return (-1);
+	}
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 	{
@@ -41,7 +64,12 @@ int	open_redout(char *filename)
 int	open_append(char *filename)
 {
 	int	fd;
-	
+
+	if (!check_filename(filename))
+	{
+	 	write(2, "minishell: ambiguous redirect\n", 31);
+		return (-1);
+	}
 	fd = open(filename, O_APPEND | O_WRONLY | O_CREAT, 0644);
 	if (fd == -1)
 	{
